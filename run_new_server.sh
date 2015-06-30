828d03181bbf37a208c7d9bbaa996d39a1f92f8b
pando85 <pando855@gmail.com>
2015-06-30 10:01:57 +0200

    Added usage and fixed spaces problem

--- run_new_server.sh
+++ run_new_server.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 set -e
-BASE_DIR=$(dirname -- $(readlink -fn -- "$0"))
+
+if [ "x$1" == x ] || [ "x$2" == x ]; then
+    echo "Usage: $0 <server-name> <server-port>" >&2
+    exit 1
+fi
 
 SERVER_NAME=$1
 SERVER_PORT=$2
-SERVER_PATH=${BASE_DIR}/../$SERVER_NAME
+
+BASE_DIR="$(dirname -- $(readlink -fn -- "$0"))"
+SERVER_PATH="${BASE_DIR}/servers/$SERVER_NAME"
 
 NEW_PASSWORD=$(date | md5sum |cut -d " " -f 1)
 
-mkdir $SERVER_PATH
+mkdir -p "$SERVER_PATH"
 
-cp -r conf $SERVER_PATH
+cp -r conf "$SERVER_PATH"
 
-sed -i "s/PASSWORD/$NEW_PASSWORD/g" $SERVER_PATH/{q3ut4/server.cfg,spunky-conf/conf/settings.conf}
+sed -i "s/PASSWORD/$NEW_PASSWORD/g" "$SERVER_PATH"/{q3ut4/server.cfg,spunky-conf/conf/settings.conf}
 
-docker run -d -v $SERVER_PATH/q3ut4:/q3ut4 -e URT_PORT=$SERVER_PORT \
+docker run -d -v "$SERVER_PATH"/q3ut4:/q3ut4 -e URT_PORT=$SERVER_PORT \
            --net host --name="$SERVER_NAME" urbanterror-server
 
-docker run -d -v $SERVER_PATH/spunky-conf:/root/spunkybot \
-           -v $SERVER_PATH/q3ut4:/root/q3ut4 --name="${SERVER_NAME}_spunkybot"\
+docker run -d -v "$SERVER_PATH"/spunky-conf:/root/spunkybot \
+           -v "$SERVER_PATH"/q3ut4:/root/q3ut4 --name="${SERVER_NAME}_spunkybot"\
            --net=host spunkybot /root/spunkybot
