ef3075b69ca0593f8e0fc5db1b84971177898d99
pando85 <pando855@gmail.com>
2015-07-01 11:01:28 +0200

    Local to global variables

--- auto-urtservers.sh
+++ auto-urtservers.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 set -e
 
-BASE_DIR="$(dirname -- $(readlink -fn -- "$0"))"
-
 # Make sure only root can run this script
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
+
+BASE_DIR="$(dirname -- $(readlink -fn -- "$0"))"
+
+SERVER_NAME="$2"
+SERVER_PORT=$3
+ 
+SERVERS_PATH="$BASE_DIR/servers"
+SERVER_PATH="$SERVERS_PATH/$SERVER_NAME"
+
+CONFIG_PATH="$BASE_DIR/conf"
+ 
+DOCKER_URT="${SERVER_NAME}_urtserver"
+DOCKER_SPUNKYBOT="${SERVER_NAME}_spunkybot"
 
 
 install(){
@@ -19,18 +30,11 @@ install(){
 
 
 init(){
-    if [ "$2" == "" ] || [ "$3" == "" ]; then
+    if [ "$SERVER_NAME" == "" ] || [ "$SERVER_PORT" == "" ]; then
         echo "Usage: $0 init <server-name> <server-port>" >&2
         exit 1
     fi
-    
-    SERVER_NAME="$2"
-    SERVER_PORT=$3
-
-    SERVERS_PATH="$BASE_DIR/servers"
-    CONFIG_PATH="$BASE_DIR/conf"
-    SERVER_PATH="$SERVERS_PATH/$SERVER_NAME"
-
+   
     NEW_PASSWORD=$(date | md5sum |cut -d " " -f 1)
     
     mkdir -p "$SERVERS_PATH"
@@ -42,29 +46,21 @@ init(){
     sed -i "s/PORT/$SERVER_PORT/g" "$SERVER_PATH/spunky-conf/conf/settings.conf"
     
     docker run -d -v "$SERVER_PATH"/q3ut4:/q3ut4 -v "$CONFIG_PATH"/maps:/maps -e URT_PORT=$SERVER_PORT \
-                    --net=host --name="${SERVER_NAME}_urtserver" urbanterror-server
+                    --net=host --name="$DOCKER_URT" urbanterror-server
     
     sleep 15
     
     docker run -d -v "$SERVER_PATH"/spunky-conf:/root/spunkybot \
-                    -v "$SERVER_PATH"/q3ut4:/root/q3ut4 --name="${SERVER_NAME}_spunkybot"\
+                    -v "$SERVER_PATH"/q3ut4:/root/q3ut4 --name="$DOCKER_SPUNKYBOT"\
                     --net=host spunkybot /root/spunkybot
 }
 
 
 remove(){
-    if [ "$2" == "" ]; then
+    if [ "$SERVER_NAME" == "" ]; then
         echo "Usage: $0 remove <server-name>" >&2
         exit 1
     fi
-    
-    SERVER_NAME="$2"
-    
-    SERVERS_PATH="$BASE_DIR/servers"
-    SERVER_PATH="$SERVERS_PATH/$SERVER_NAME"
-    
-    DOCKER_URT="${SERVER_NAME}_urtserver"
-    DOCKER_SPUNKYBOT="${SERVER_NAME}_spunkybot"
     
     rm -rf "$SERVER_PATH"
     
@@ -79,9 +75,9 @@ remove(){
 if [ "$1" = "install" ]; then
     install
 elif [ "$1" = "init" ]; then
-    init "$@"
+    init
 elif [ "$1" = "remove" ]; then
-    remove "$@"
+    remove
 else
     echo "Usage: $0 install|init|remove" >&2
     exit 1
